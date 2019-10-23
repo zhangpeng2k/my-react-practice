62192c1369051af01b553c83a84ef7ea8b5457c0
zhangpeng2k <1028158677@qq.com>
2019-10-23 21:42:52 +0800

    1

--- src/views/flex/index.js
+++ src/views/flex/index.js
@@ -1,1 +1,11 @@
 import React, { Component, Fragment } from 'react'
+
+class FlexDemo extends Component {
+    render(){
+        return(
+            <h1>哇</h1>
+        )
+    }
+}
+
+export default FlexDemo
