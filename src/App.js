4fe71388baca12a0e7092913afc4396a15fd9fbe
zhangpeng2k <1028158677@qq.com>
2019-10-23 21:36:18 +0800

    todolist

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Todo from './views/todo/index.js'
 
 function App() {
   return (
+    <>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -20,6 +22,8 @@ function App() {
         </a>
       </header>
     </div>
+    <Todo></Todo>
+    </>
   );
 }
 
